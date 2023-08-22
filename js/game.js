2b64a851381db16349c205a8d4146c37b8b88bfa
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-08-22 21:37:22 +0100

    Moved notes to separate file. Refactored code to be a lot more readable so I don't go insane. Some logic broken due to refactor. Added ternary operator to determine CPU symbol.

--- js/game.js
+++ js/game.js
@@ -1,51 +1,26 @@
-/*
-    The main goal of this project is to have as little global code
-    as possible.
+const gameState = {
+    gameboard: ["", "", "", "", "", "", "", "", ""],
+    players: {
+        human: {name: "", symbol: "X"},
+        cpu: {name: "CPU", symbol: "O"}
+    }
 
-    Tips:
+    // Ternary operator makes the cpuSymbol the opposite of the human player's symbol.
+    // If playerSymbol is "X" then cpuSymbol is "O", otherwise cpuSymbol is "X".
+    // let cpuSymbol = humanPlayer.playerSymbol === "X" ? "O" : "X"; // temp while I work out new refactored logic.
+}
 
-        Think of Factory Functions as "Object Creators"
-
-        A factory function at the most basic level is just a function that
-        returns an object, that's it.
-        When you call this function, it gives you an object back. The beauty
-        of this is that you can customise the obejct for different situations
-        each time you call the function.
-
-        If you only need one of something (gameBoard, displayController),
-        use a module.
-
-        If you need multiples of something (players), create
-        them with factories.
-
-        Keep as much as possible private (not exported), exposing only what
-        is necessary.
-
-        Ensure that related functionalities are bundles together.
-*/
-
-/*     I've gone for arrow functions here as I prefer them and do not
-    need the "this" keyword.
-
-    The main difference between arrow functions and traditional
-    function declarations is that arrow functions don't have their own
-    "this", they inherit "this" from the enclsosing scope.
-
-    Tranditional function declarations have their own "this" binding,
-    which is typically set by how they are called. */
-
-const Gameboard = {
-    gameboard: [
-        "", "", "",
-        "", "", "",
-        "", "", ""
-    ]
+// Factory function for creating players.
+const createPlayer = (name, symbol) => {
+    return {
+        name,
+        symbol
+    };
 }
 
 // Module containing functions for when the game is actually running.
-const PlayGame = (() => {
-    // Private functions
-    
+const gameLogic = (() => {
+
     const checkMoveValidity = (currentCellIndex) => {
         if (Gameboard.gameboard[currentCellIndex] === "") {
             // Checks if the current index in the gameboard array is an empty string.
@@ -56,6 +31,27 @@ const PlayGame = (() => {
             console.log("Move is invalid");
             return false;
         }
+    }
+
+    const makeMove = (humanPlayer) => {
+        const gameCells = document.querySelectorAll(".board-cell");
+
+        gameCells.forEach(cell => {
+            cell.addEventListener("click", () => {
+                
+                // Grab the index of the cell that was clicked & convert to number.
+                const currentCellIndex = Number(cell.dataset.index);
+                console.log(currentCellIndex);
+
+                checkMoveValidity(currentCellIndex);
+                Gameboard.gameboard.splice(currentCellIndex, 1, humanPlayer.playerSymbol);
+
+                console.log(Gameboard.gameboard);
+                updateCells(); // Assign current player's X or O to that cell.
+                checkWin();
+                cpuPlayer.cpuEasyMove();
+            });
+        });
     }
 
     const checkWin = () => {
@@ -80,99 +76,70 @@ const PlayGame = (() => {
         });
     }
 
-    const makeMove = () => {
-        const gameCells = document.querySelectorAll(".board-cell");
+    return {
+        checkMoveValidity,
+        makeMove,
+        checkWin
+    };
 
-        gameCells.forEach(cell => {
-            cell.addEventListener("click", () => {
-                
-                // Grab the index of the cell that was clicked & convert to number.
-                const currentCellIndex = Number(cell.dataset.index);
-                console.log(currentCellIndex);
+})(); // IIFE (Immediately Invoked Function Expression)
 
-                checkMoveValidity(currentCellIndex);
-                Gameboard.gameboard.splice(currentCellIndex, 1, "X");
 
+// Module containing logic for CPU moves, two difficulties, easy and hard.
+const cpuLogic = (() => {
+
+    const cpuEasyMove = () => {
+        // Easy it a random number so the cpu does not actively try to win.
+        // Use random number from 0 to 8 to get random move.
+        // Check if number is a valid move.
+
+        console.log("CPU Making Easy Move");
+
+        const getRandomMove = () => {
+            let randomMove = Math.random * 10;
+            console.log(`Random number: ${randomMove}`);
+            let currentMove = getRandomMove.randomMove;
+            return currentMove;
+        }
+        
+        if (getRandomMove.currentMove > 8) {
+            // Number is invalid
+            console.log(`CPU move was ${getRandomMove.currentMove}`);
+            console.log("CPU move was invalid, rerunning...")
+            getRandomMove(); // Re-gen the move.
+        } else {
+            checkMoveValidity(); // Number is valid, check move validity.
+            if (checkMoveValidity == true) {
+                Gameboard.gameboard.splice(currentCellIndex, 1, cpuSymbol);
+                updateCells(getRandomMove.currentMove);
+                console.log(`Move came back valid: ${getRandomMove.currentMove}`);
                 console.log(Gameboard.gameboard);
-                updateCells(); // Assign current player's X or O to that cell.
-                checkWin();
-                cpuPlayer.cpuEasyMove();
-            });
-        });
-    }
-
-    const cpuPlayer = (() => {
-        const cpuPlayerSymbol = "O";
-
-        const cpuEasyMove = () => {
-            // Easy it a random number so the cpu does not actively try to win.
-            // Use random number from 0 to 8 to get random move.
-            // Check if number is a valid move.
-
-            console.log("CPU Making Easy Move");
-
-            const getRandomMove = () => {
-                let randomMove = Math.random * 10;
-                console.log(`Random number: ${randomMove}`);
-                let currentMove = getRandomMove.randomMove;
-                return currentMove;
-            }
-            
-            if (getRandomMove.currentMove > 8) {
-                // Number is invalid
-                console.log(`CPU move was ${getRandomMove.currentMove}`);
-                console.log("CPU move was invalid, rerunning...")
-                getRandomMove(); // Re-gen the move.
             } else {
-                checkMoveValidity(); // Number is valid, check move validity.
-                if (checkMoveValidity == true) {
-                    Gameboard.gameboard.splice(currentCellIndex, 1, "O");
-                    updateCells(getRandomMove.currentMove);
-                    console.log(`Move came back valid: ${getRandomMove.currentMove}`);
-                    console.log(Gameboard.gameboard);
-                } else {
-                    console.log("CPU move was invalid, rerunning...")
-                }
+                console.log("CPU move was invalid, rerunning...")
             }
         }
-        const cpuHardMove = () => {
-            // Hard, the AI actively tries to win by blocking the player.
-            // But it's boring to never be able to win so there must be a random
-            // chance that the AI makes a move that is random. This makes winning rare but possible.
-
-            // Check the current status of the array.
-        }
-
-        return {cpuEasyMove};
-    })(); // IIFE
-
-    const updateCells = () => {
-        Gameboard.gameboard.forEach(currentIndex => {
-
-            // currentIndex is null? need to look at why, probably just an issue of when the updateCells function is called.
-            let currentCell = document.querySelector(`[data-index="${currentIndex}"]`);
-
-            if (currentIndex === "X") {
-                // Update cell with image of Panda.
-                currentCell.innerHTML = `<img src="/assets/icons/panda-bear-panda-svgrepo-com.svg" alt="">`;
-            } else if (currentIndex === "O") {
-                // Update cell with image of Raccoon.
-                currentCell.innerHTML = `<img src="/assets/icons/raccoon-svgrepo-com.svg" alt="">`;
-            }
-        });
     }
-    return {makeMove};
+    const cpuHardMove = () => {
+        // Hard, the AI actively tries to win by blocking the player.
+        // But it's boring to never be able to win so there must be a random
+        // chance that the AI makes a move that is random. This makes winning rare but possible.
+
+        // Check the current status of the array.
+    }
 })(); // IIFE
 
-const startGame = (() => {
-    const btnPlay = document.querySelector("#btn-play");
-    const modal = document.querySelector("#modal");
-    const nameInput = document.querySelector("#player-name");
+const domController = (() => {
 
-    // Factory function for creating players.
-    const createPlayer = () => {
-        const playerSymbol = "X"
-        return playerSymbol;
+    const startGame = () => {
+        const btnPlay = document.querySelector("#btn-play");
+        const modal = document.querySelector("#modal");
+        const nameInput = document.querySelector("#player-name");
+    
+        
+        btnPlay.addEventListener("click", e => {
+            modal.showModal();
+            createBoard();
+        });
     }
 
     const createBoard = () => {
@@ -212,12 +179,31 @@ const startGame = (() => {
             PlayGame.makeMove(humanPlayer);
         });
     }
-    
-    btnPlay.addEventListener("click", e => {
-        modal.showModal();
-        createBoard();
-    });
 
-    return modal; // now in the public scope
+    const updateCells = () => {
 
-})(); // IIFE (Immediately invoked function expression)
+        // The forEach method, takes two arguments, the first is the element in the array, the second is the index
+        // of that element.
+        Gameboard.gameboard.forEach((currentValue, currentIndex) => {
+
+            // currentIndex is null? need to look at why, probably just an issue of when the updateCells function is called.
+            let currentCell = document.querySelector(`[data-index="${currentIndex}"]`);
+
+            if (currentValue === "X") {
+                // Update cell with image of Panda.
+                currentCell.innerHTML = `<img src="/assets/icons/panda-bear-panda-svgrepo-com.svg" alt="">`;
+            } else if (currentValue === "O") {
+                // Update cell with image of Raccoon.
+                currentCell.innerHTML = `<img src="/assets/icons/raccoon-svgrepo-com.svg" alt="">`;
+            }
+        });
+    }
+
+    return {
+        startGame,
+        createBoard,
+        updateCells
+    };
+})(); // IIFE
+
+domController.startGame();
